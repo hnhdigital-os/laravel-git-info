c5a98d90744a0ad5a7663e63ad9cdbaec5ea44d1
Rocco Howard <rocco@bluora.com.au>
2016-08-14 01:42:57 +1000

    Added env path for git; Added commits and commits behind lookups

--- src/GitInfo.php
+++ src/GitInfo.php
@@ -14,7 +14,7 @@ class GitInfo
     {
         $dir = getcwd();
         chdir(base_path());
-        $output = shell_exec('git '.$command);
+        $output = shell_exec(env('GIT_INFO_PATH', 'git').' '.$command);
         chdir($dir);
         return trim($output);
     }
@@ -30,6 +30,37 @@ class GitInfo
     }
 
     /**
+     * Get total commits.
+     * 
+     * @return string
+     */
+    public function commits()
+    {
+        return $this->git('rev-list HEAD --count');
+    }
+
+    /**
+     * Get total commits behind a given branch.
+     * 
+     * @return string
+     */
+    public function commitsBehind($branch = 'master', $return_text = true)
+    {
+        if ($this->branch() == $branch) {
+            return ($return_text) ? '' : 0;
+        }
+        $current_count = $this->commits();
+        $branch_count = $this->git('rev-list '.$branch.' --count');
+        $diff_count = $current_count - $branch_count;
+        if ($current_count > $branch_count) {
+            return ($return_text) ? $diff_count .' commits ahead of '.$branch : $diff_count;
+        } elseif ($branch_count > $current_count) {
+            return ($return_text) ? abs($diff_count) .' commits behind of '.$branch : abs($diff_count);
+        }
+        return 0;
+    }
+
+    /**
      * Get the current version.
      * 
      * @return string
@@ -39,3 +70,5 @@ class GitInfo
         return $this->git('describe --always --tags --dirty');
     }
 }
+
+
